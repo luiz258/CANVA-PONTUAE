a49839b49f71eb51b9f680ed37d28915f7836bf4
Wandeson Da Silva Batista <wandeson365@gmail.com>
2020-11-02 20:29:24 -0300

    Interface

---
+++ PontuaAe.Dominio/FidelidadeContexto/Repositorios/Servicos/ApiChatproWhatsapp/IChatproWhatsApp.cs
@@ -0,0 +1,18 @@
+﻿using PontuaAe.Dominio.FidelidadeContexto.Consulta.MarketingConsulta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontuaAe.Dominio.FidelidadeContexto.Repositorios.Servicos.ApiChatproWhatsapp
+{
+    public interface IChatproWhatsApp
+    { 
+        Task Enviar_mensagemDaPontuacao(string Contato, string Conteudo);
+        Task<List<string>> EnviarMensagemEmMassa(IEnumerable<ListaContatosPorSegCustomizado> ListContatos, string Conteudo);
+        Task<List<string>> EnviarMensagemEmMassa(IEnumerable<ObterAutomacaoTipoDiaSemana> listaDadosAutomacao, string Conteudo, string DataEnvio, string HoraEnvio);
+        Task<List<string>> EnviarMensagemEmMassa(IEnumerable<ObterAutomacaoTipoUltimaFide> listDadosAutomacao, string DataEnvio, string HoraEnvio);
+        Task<string> EnviarMensagemEmMassa(string Contato, string NomeCompleto, string Conteudo, string DataEnvio, string HoraEnvio);
+    } 
+}
