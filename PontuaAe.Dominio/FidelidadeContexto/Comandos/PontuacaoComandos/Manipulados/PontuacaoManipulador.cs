f5474f32f6d9f4a0f03769c3b1789c96323dca3e
Wandeson Da Silva Batista <wandeson365@gmail.com>
2020-11-04 23:30:46 -0300

    alterei o numero do whatsap para link do whatsapp

--- PontuaAe.Dominio/FidelidadeContexto/Comandos/PontuacaoComandos/Manipulados/PontuacaoManipulador.cs
+++ PontuaAe.Dominio/FidelidadeContexto/Comandos/PontuacaoComandos/Manipulados/PontuacaoManipulador.cs
@@ -142,13 +142,14 @@ namespace PontuaAe.Dominio.FidelidadeContexto.Comandos.PontuacaoComandos.Manipul
                             var data = data_.ToString();
                             var n = comando.Contato;
                             string _numero = n;
-                        string linkWhatsapEmpresa = await _empresaRepositorio.ObterDados(campo.Telefone);
+                        dynamic linkWhatsapEmpresa_ = await _empresaRepositorio.ObterDados(campo.LinkWhatSapp);
+                        string linkWhatsapEmpresa = Convert.ToString(linkWhatsapEmpresa_);
                         string conteudo = $"*{campo.NomeFantasia}*:" + "Você ganhou " + $"{ponto} pontos" + " em " + $"{data}" + @"\r\n" +
                         @"\r\n Seu saldo atual é de " + $"{novoSaldo} " + "pontos" + @"\r\n" +
                         @"\r\n Quando achar conveniente, basta" + @"\r\n" +
                         @"\r\n solicitar o resgate dos seus pontos \r\n" + @" no caixa!" + @"\r\n" +
                         @"\r\n Obrigado pela preferência! :)" + @"\r\n" +
-                        @"\r\n 💬 *Chame a empresa MAISS no Whats, tocando aqui* 👇" + @"\r\n" + $"{linkWhatsapEmpresa}" +
+                        @"\r\n 💬 *Chame a empresa MAISS no Whats, tocando aqui* 👇 " + @"\r\n" + $"{linkWhatsapEmpresa} " +
                         @"\r\n ⚠ *POR GENTILEZA COMPLETE SEU CADASTRO AQUI* 👇" + @"\r\n http://pontuaae.herokuapp.com/registerCustomer" +
                         @"\r\n *Para acessar sua conta, toque no link abaixo 👇 é informe EMAIL e SENHA depois clique em ENTRAR.* \r\n" + @"\r\n http://pontuaae.herokuapp.com/loginCliente" + @"\r\n";
                         //@"\r\n 🎁 *PRÊMIOS que você pode está resgatando ao completa o saldo de pontos necessário:* \r\n" + $"{item};";
